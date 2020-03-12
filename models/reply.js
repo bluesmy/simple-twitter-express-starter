2b7769e1b50ddff365b16fec1688e2d311b67edb
Miao Yueh Su <bluesmy1020@gmail.com>
2020-03-12 21:34:54 +0800

    Create model association

--- models/reply.js
+++ models/reply.js
@@ -1,8 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Reply = sequelize.define('Reply', {
+    comment: DataTypes.STRING,
+    TweetId: DataTypes.INTEGER,
+    UserId: DataTypes.INTEGER
   }, {});
-  Reply.associate = function(models) {
+  Reply.associate = function (models) {
+    Reply.belongsTo(models.Tweet)
+    Reply.belongsTo(models.User)
   };
   return Reply;
 };
