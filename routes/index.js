28329c816dd21b29b4fe676d8345a7393782ac91
Emily Liu <a62262002@gmail.com>
2020-03-15 21:56:56 +0800

    add main nav bar

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,37 @@
-const express = require('express')
-const router = express.Router()
-const helpers = require('../_helpers');
+const express = require("express");
+const router = express.Router();
+const helpers = require("../_helpers");
 
-const userController = require('../controllers/userController.js')
+const userController = require("../controllers/userController.js");
+const tweetController = require("../controllers/tweetController.js");
 
-const passport = require('../config/passport')
+const passport = require("../config/passport");
 
 const authenticated = (req, res, next) => {
   if (helpers.ensureAuthenticated(req)) {
-    return next()
+    return next();
   }
-  req.flash('error_messages', '請先登入才能使用')
-  res.redirect('/signin')
-}
+  req.flash("error_messages", "請先登入才能使用");
+  res.redirect("/signin");
+};
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
-router.get('/', authenticated, (req, res) => res.render('tweets'))
+router.get("/", authenticated, (req, res) => res.redirect("/tweets"));
+router.get("/tweets", authenticated, tweetController.getTweets);
 
-router.get('/signup', userController.signUpPage)
-router.post('/signup', userController.signUp)
+router.get("/signup", userController.signUpPage);
+router.post("/signup", userController.signUp);
 
-router.get('/signin', userController.signInPage)
-router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
-router.get('/logout', userController.logout)
+router.get("/signin", userController.signInPage);
+router.post(
+  "/signin",
+  passport.authenticate("local", {
+    failureRedirect: "/signin",
+    failureFlash: true
+  }),
+  userController.signIn
+);
+router.get("/logout", userController.logout);
 
-module.exports = router
+module.exports = router;
