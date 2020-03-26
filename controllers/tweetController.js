01da57d779e0e1d87569c78d8ab525ebb4a2fa57
Emily Liu <a62262002@gmail.com>
2020-03-26 21:45:51 +0800

    update the main layout, tweetcontroller, tweets

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -1,29 +1,30 @@
-const db = require("../models");
-const Tweet = db.Tweet;
-const User = db.User;
+const db = require('../models')
+const Tweet = db.Tweet
+const User = db.User
+const helpers = require('../_helpers')
 const tweetController = {
   getTweets: (req, res) => {
     return Tweet.findAll({
-      order: [["createdAt", "DESC"]],
+      order: [['createdAt', 'DESC']],
       include: [User]
     }).then(tweets => {
       User.findAll({
         limit: 10,
-        include: [Tweet, { model: User, as: "Followers" }]
+        include: [Tweet, { model: User, as: 'Followers' }]
       }).then(users => {
         users = users.map(user => ({
           ...user.dataValues,
           FollowerCount: user.Followers.length,
           isFollowed: req.user.Followings.map(d => d.id).includes(user.id),
           introduction: user.introduction.substring(0, 50)
-        }));
-        users = users.sort((a, b) => b.FollowerCount - a.FollowerCount);
-        return res.render("tweets", {
+        }))
+        users = users.sort((a, b) => b.FollowerCount - a.FollowerCount)
+        return res.render('tweets', {
           users: users,
           tweets: tweets
-        });
-      });
-    });
+        })
+      })
+    })
   },
 
   // getTweets: (req, res) => {
@@ -44,18 +45,18 @@ const tweetController = {
       req.body.description.length > 140 ||
       req.body.description.length === 0
     ) {
-      req.flash("error_messages", "please enter 140 words.");
-      return res.redirect("back");
+      req.flash('error_messages', 'please enter 1 to 140 words.')
+      return res.redirect('/tweets')
     } else {
       return Tweet.create({
         description: req.body.description,
-        UserId: req.user.id
+        UserId: helpers.getUser(req).id
       }).then(tweet => {
-        req.flash("success_messages", "tweet was successfully created");
-        res.redirect("/tweets");
-      });
+        req.flash('success_messages', 'tweet was successfully created')
+        res.redirect('/tweets')
+      })
     }
   }
-};
+}
 
-module.exports = tweetController;
+module.exports = tweetController
