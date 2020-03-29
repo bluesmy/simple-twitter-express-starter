c585d2c82b05ea91e308e515ff1017d6205ca856
bluesmy <43344912+bluesmy@users.noreply.github.com>
2020-03-29 16:46:10 +0800

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,15 +1,15 @@
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcryptjs");
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
 
-const db = require("../models");
+const db = require('../models')
 const User = db.User;
 
 passport.use(
   new LocalStrategy(
     {
-      usernameField: "email",
-      passwordField: "password",
+      usernameField: 'email',
+      passwordField: 'password',
       passReqToCallback: true
     },
     (req, username, password, done) => {
@@ -18,33 +18,33 @@ passport.use(
           return done(
             null,
             false,
-            req.flash("error_messages", "That email is not registered")
-          );
+            req.flash('error_messages', 'That email is not registered')
+          )
         if (!bcrypt.compareSync(password, user.password))
           return done(
             null,
             false,
-            req.flash("error_messages", "密碼輸入錯誤！")
-          );
-        return done(null, user);
-      });
+            req.flash('error_messages', '密碼輸入錯誤！')
+          )
+        return done(null, user)
+      })
     }
   )
-);
+)
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+  done(null, user.id)
+})
 
 passport.deserializeUser((id, done) => {
   User.findByPk(id, {
     include: [
-      { model: User, as: "Followers" },
-      { model: User, as: "Followings" }
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
     ]
   }).then(user => {
-    return done(null, user);
-  });
-});
+    return done(null, user)
+  })
+})
 
-module.exports = passport;
+module.exports = passport
