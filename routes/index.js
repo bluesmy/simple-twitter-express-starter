f135becf0d4cc8575cd72392eca564361d7bc38e
bluesmy <43344912+bluesmy@users.noreply.github.com>
2020-03-31 13:21:23 +0800

    Revert "Feature/add tweets"

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const helpers = require('../_helpers')
 
 const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
-const tweetController = require('../controllers/tweetController.js')
 
 const passport = require('../config/passport')
 
@@ -12,7 +11,7 @@ const authenticated = (req, res, next) => {
   if (helpers.ensureAuthenticated(req)) {
     return next()
   }
-  req.flash('error_messages', "請先登入才能使用")
+  req.flash('error_messages', '請先登入才能使用')
   res.redirect('/signin')
 }
 
@@ -26,10 +25,7 @@ const authenticatedAdmin = (req, res, next) => {
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
-
 router.get('/', authenticated, (req, res) => res.redirect('/tweets'))
-router.get('/tweets', authenticated, tweetController.getTweets)
-router.post('/tweets', authenticated, tweetController.postTweets)
 
 router.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/tweets'))
 router.get('/admin/tweets', authenticatedAdmin, adminController.getTweets)
@@ -41,16 +37,7 @@ router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 
 router.get('/signin', userController.signInPage)
-router.post(
-  '/signin',
-  passport.authenticate('local', {
-    failureRedirect: '/signin',
-    failureFlash: true
-  }),
-  userController.signIn
-)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
-
-router.get('/users/:id/tweets', authenticated, userController.getUser)
 
 module.exports = router
